eb589ea946ca8c036779f1d187224d3a395419e2
Brian Clozel <bclozel@gopivotal.com>
2014-02-21 14:23:52 +0100

    First version - 0.0.1

---
+++ test/main.js
@@ -0,0 +1,70 @@
+'use strict';
+var gutil = require('gulp-util');
+var es = require("event-stream");
+var path = require("path");
+var should = require("should");
+var bowerSrc = require('../index');
+
+describe('bowerSrc()', function () {
+
+    function streamFromConfig(path) {
+        return bowerSrc({
+            paths: {
+                bowerJson: __dirname + "/" + path,
+                bowerrc: __dirname + "/.bowerrc"
+            }
+        });
+    }
+
+    function expect(filenames) {
+        var expectedFiles = [].concat(filenames).map(function(filename) {
+            return path.join(__dirname, filename);
+        });
+
+        function run(path, done) {
+            var stream = streamFromConfig(path);
+            var srcFiles = [];
+
+            stream.on("end", function(){
+                srcFiles.should.be.eql(expectedFiles);
+                done();
+            });
+
+            stream.pipe(es.map(function(file, callback){
+                srcFiles.push(file.path);
+                callback();
+            }));
+        }
+
+        return {
+            fromConfig: function(path) {
+                return {
+                    when: function(done) { run(path, done); }
+                }
+            }
+        }
+    }
+
+    it('should ignore the expected files', function (done) {
+        expect([
+            "/fixtures/simple",
+            "/fixtures/simple/main.js",
+            "/fixtures/overwritten",
+            "/fixtures/overwritten/main.js",
+            "/fixtures/empty",
+            "/fixtures/empty/main.js"
+        ]).fromConfig("simple.json").when(done);
+    });
+
+    it("should throw an exception when using negate glob patterns in ignore", function(done) {
+        try {
+            streamFromConfig("negate.json");
+
+            should.fail("due to the use of a negate glob pattern.");
+        } catch (e) {
+            e.message.should.containEql("does not support ignore glob patterns - please override ");
+            done();
+        }
+    });
+
+});
